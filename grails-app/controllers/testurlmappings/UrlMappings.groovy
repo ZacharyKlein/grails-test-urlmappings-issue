7c4cae9428f268d0519539e47c1609ca8e582012
Zachary Klein <zak@silver-chalice.com>
2018-01-23 12:57:39 -0600

    restored default url mappings

--- grails-app/controllers/testurlmappings/UrlMappings.groovy
+++ grails-app/controllers/testurlmappings/UrlMappings.groovy
@@ -3,8 +3,17 @@ package testurlmappings
 class UrlMappings {
 
     static mappings = {
+        "/$controller/$action?/$id?(.$format)?" {
+            constraints {
+                // apply constraints here
+            }
+        }
+
+
         get "/custom/mapping"(controller: 'test', action: 'index')
 
         "/"(view: "/index")
+        "500"(view: '/error')
+        "404"(view: '/notFound')
     }
 }
